808ed11abf463b947cf66b61d4fddb212e3c4362
PaingPainghein <125305378+PaingPainghein@users.noreply.github.com>
2025-03-23 05:52:32 +0000

    up

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def response():
     return jsonify({"response": response_text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
