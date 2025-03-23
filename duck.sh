3fa39698865fcdba50c364a7d84ae6bdbcc0e1a4
PaingPainghein <125305378+PaingPainghein@users.noreply.github.com>
2025-03-23 06:09:15 +0000

    iu

--- duck.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DOMAIN="pphaibot"  # Duck DNS subdomain (".duckdns.org" မထည့်ပါ)
-TOKEN="b35bcc09-d172-4dce-9d38-43942692dc2e"  # သင့် token
-echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl -o ~/duckdns/duck.log -K -
